aa435e906c4f9860cc54a4bf6593a554078f1f0b
Praise Obah <obahsomto@gmail.com>
2024-11-12 14:23:35 -0800

    animation

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/Login/Login.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Login from "./Login.js";
+import { StyleSheetTestUtils } from "aphrodite";
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Verify it renders without crashing", () => {
+  test("should verify is Login component renders", () => {
+    const wrapper = shallow(<Login />);
+  });
+
+  test("should very 2 inputs and 2 label tags", () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find("input").length).toBe(2);
+    expect(wrapper.find("label").length).toBe(2);
+  });
+});
